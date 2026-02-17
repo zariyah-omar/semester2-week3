d5b99a824845fd721af1e92b779b047dc560e320
zariyah-omar <mxbx0968@leeds.ac.uk>
2026-02-17 16:49:40 +0000

    weeek 3

--- session-1/task3/hello1.c
+++ session-1/task3/hello1.c
@@ -1,14 +1,8 @@
-
-/*
- * Hello world program 
- * - compilation errors
- */
-
 #include <stdio.h>
 
 int main( void ) {
     
-    printf("Hello\n"); // Semicolon added
+    printf("Hello\n"); // Added the semicolon here
 
     return 0;    
 }
